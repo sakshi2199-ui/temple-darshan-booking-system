68aa3d19a1f60d39481823b84135bb2750298895
sakshi2199-ui <sakshikumari19032004@gmail.com>
2026-03-16 22:17:37 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth",require("./routes/authRoutes"));
+app.use("/api/temples",require("./routes/templeRoutes"));
+app.use("/api/bookings",require("./routes/bookingRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>console.log("Server running"));
